b143bf8a101d49ac84a1f333fa111bd8869c8ab8
Uk-jake <mag0225@naver.com>
2024-04-10 15:57:10 +0900

    feat: Springboot 초기환경세팅 완료.

---
+++ hanformproject/src/main/java/com/example/hanformproject/HanformprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanformproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanformprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanformprojectApplication.class, args);
+	}
+
+}
---
+++ hanformproject/src/test/java/com/example/hanformproject/HanformprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanformproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanformprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
