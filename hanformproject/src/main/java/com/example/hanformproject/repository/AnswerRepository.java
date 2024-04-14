f33e7d7ed02dcf14d8220fc92891acc4a15a38f7
김동욱 <bradl0809@naver.com>
2024-04-14 20:13:46 +0900

    fix : 필요없는 import 삭제

--- hanformproject/src/main/java/com/example/hanformproject/repository/AnswerRepository.java
+++ hanformproject/src/main/java/com/example/hanformproject/repository/AnswerRepository.java
@@ -1,8 +1,9 @@
 package com.example.hanformproject.repository;
 
 import com.example.hanformproject.entity.AnswerEntity;
-import com.example.hanformproject.entity.OptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+
 }
+
