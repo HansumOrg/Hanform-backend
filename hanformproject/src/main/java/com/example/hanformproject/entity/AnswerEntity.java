4c7159d7a6456626ab7a419d5f6f15dcb915b5d7
김동욱 <bradl0809@naver.com>
2024-04-14 15:14:20 +0900

    feat : AnswerEntity 추가

---
+++ hanformproject/src/main/java/com/example/hanformproject/entity/AnswerEntity.java
@@ -0,0 +1,32 @@
+package com.example.hanformproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answerId", nullable = false)
+    private Long answerId;
+
+    @OneToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "questionId", nullable = false)
+    private QuestionEntity question;
+
+    @Column(name= "answerText", nullable = false)
+    private String answerText;
+
+}
